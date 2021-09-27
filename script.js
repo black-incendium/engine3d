3b92e1d4d424e4c6e49c0522b39cf9cf698da71c
black-incendium <cryingincendium@gmail.com>
2021-09-27 13:36:23 +0200

    added files

---
+++ script.js
@@ -0,0 +1,241 @@
+/* function rotate({x1, y1, z1, x2, y2, z2, angle}) {
+    let d = Math.sqrt(x2**2 + y2**2 + z2**2);
+    [x2, y2, z2] = [x2/d, y2/d, z2/d];
+    let q = {
+        r: Math.cos(angle/2),
+        i: Math.sin(angle/2)*x2,
+        j: Math.sin(angle/2)*y2,
+        k: Math.sin(angle/2)*z2,
+    }
+    let newX = x1*(1-2*q.j**2-2*q.k**2) + y1*(q.i*q.j-q.k*q.r)*2 + z1*(q.i*q.k+q.j*q.r)*2;
+    let newY = x1*(q.i*q.j+q.k*q.r)*2 + y1*(1-2*q.i**2-2*q.k**2) + z1*(q.j*q.k-q.i*q.r)*2;
+    let newZ = x1*(q.i*q.k-q.j*q.r)*2 + y1*(q.j*q.k+q.i*q.r)*2 + z1*(1-2*q.i**2-2*q.j**2);
+    return {x: +newX.toFixed(10), y: +newY.toFixed(10), z: +newZ.toFixed(10)}
+} */
+
+/* let div = document.querySelector('div');
+
+ */
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth*2;
+canvas.height = window.innerHeight*2;
+
+let glob = (function(){
+    let fov = Math.PI/2;
+    let distance = canvas.width/3;
+    let sensitivity = 1/300;
+    let pressedKeys = {};
+    let moveSensitivity = 1/10;
+
+    return {
+        fov: fov,
+        distance: distance,
+        sensitivity: sensitivity,
+        pressedKeys: pressedKeys,
+        moveSensitivity: moveSensitivity
+    }
+})()
+
+let environment = (function(){
+    let objects = [];
+    let top = {x:0, y:1, z:0};
+    let front = {x:0, y:0, z:1};
+    let right = {x:1, y:0, z:0};
+    let actualAngle = 0;
+    let actualPosition = {x:0,y:0,z:0}
+
+    function newCube(center, size) {
+        let cube = {
+            points: [
+                {x:center.x-size/2,y:center.y-size/2,z:center.z-size/2},
+                {x:center.x-size/2,y:center.y-size/2,z:center.z+size/2},
+                {x:center.x-size/2,y:center.y+size/2,z:center.z-size/2},
+                {x:center.x-size/2,y:center.y+size/2,z:center.z+size/2},
+                {x:center.x+size/2,y:center.y-size/2,z:center.z-size/2},
+                {x:center.x+size/2,y:center.y-size/2,z:center.z+size/2},
+                {x:center.x+size/2,y:center.y+size/2,z:center.z-size/2},
+                {x:center.x+size/2,y:center.y+size/2,z:center.z+size/2},
+            ]
+        }
+        cube.lines = [
+            {from: 0, to: 1},
+            {from: 0, to: 2},
+            {from: 1, to: 3},
+            {from: 2, to: 3},
+            {from: 4, to: 5},
+            {from: 4, to: 6},
+            {from: 5, to: 7},
+            {from: 6, to: 7},
+            {from: 0, to: 4},
+            {from: 1, to: 5},
+            {from: 2, to: 6},
+            {from: 3, to: 7},
+        ]
+        objects.push(cube);
+    }
+
+    function rotateAround({point, axis, angle}) {
+        let d = Math.sqrt(axis.x**2 + axis.y**2 + axis.z**2);
+        [axis.x, axis.y, axis.z] = [axis.x/d, axis.y/d, axis.z/d];
+        let q = {
+            r: Math.cos(angle/2),
+            i: Math.sin(angle/2)*axis.x,
+            j: Math.sin(angle/2)*axis.y,
+            k: Math.sin(angle/2)*axis.z,
+        }
+        let newX = point.x*(1-2*q.j**2-2*q.k**2) + point.y*(q.i*q.j-q.k*q.r)*2 + point.z*(q.i*q.k+q.j*q.r)*2;
+        let newY = point.x*(q.i*q.j+q.k*q.r)*2 + point.y*(1-2*q.i**2-2*q.k**2) + point.z*(q.j*q.k-q.i*q.r)*2;
+        let newZ = point.x*(q.i*q.k-q.j*q.r)*2 + point.y*(q.j*q.k+q.i*q.r)*2 + point.z*(1-2*q.i**2-2*q.j**2);
+        return {x: +newX.toFixed(10), y: +newY.toFixed(10), z: +newZ.toFixed(10)}
+    }
+
+    function calcObjPos(obj) {
+        obj.renderedPoints = obj.points.map(function(el){
+            return {
+                x: el.x/el.z/Math.tan(glob.fov/2)*glob.distance,
+                y: -el.y/el.z/Math.tan(glob.fov/2)*glob.distance,
+                z: el.z 
+            }
+        });
+        return obj;
+    }
+
+    function rotateY(angle) {
+        objects.forEach(function(o){
+            o.points = o.points.map(function(p){
+                return rotateAround({point: p, axis: top, angle: -angle});
+            });
+        });
+    }
+
+    function rotateX(angle) {
+        if (actualAngle+angle>Math.PI/2) angle = Math.PI/2-actualAngle;
+        else if (actualAngle+angle<-Math.PI/2) angle = -Math.PI/2-actualAngle;
+        objects.forEach(function(o){
+            o.points = o.points.map(function(point){
+                return {
+                    x: point.x,
+                    y: point.y*Math.cos(angle)+point.z*Math.sin(angle),
+                    z: point.z*Math.cos(angle)-point.y*Math.sin(angle)
+                }
+            });
+        });
+        top = {
+            x: top.x,
+            y: top.y*Math.cos(angle)+top.z*Math.sin(angle),
+            z: top.z*Math.cos(angle)-top.y*Math.sin(angle)
+        }
+        right = {
+            x: right.x,
+            y: right.y*Math.cos(angle)+right.z*Math.sin(angle),
+            z: right.z*Math.cos(angle)-right.y*Math.sin(angle)
+        }
+        front = {
+            x: front.x,
+            y: front.y*Math.cos(angle)+front.z*Math.sin(angle),
+            z: front.z*Math.cos(angle)-front.y*Math.sin(angle)
+        }
+        actualAngle+=angle;
+    }
+
+    function move({x = 0, y = 0, z = 0}) {
+        objects.forEach(function(o){
+            o.points.forEach(function(p){
+                p.x -= right.x*x + front.x*z + top.x*y
+                p.y -= right.y*x + front.y*z + top.y*y
+                p.z -= right.z*x + front.z*z + top.z*y
+            })
+        });
+        actualPosition.x += x;
+        actualPosition.y += y;
+        actualPosition.z += z;
+    }
+
+    //newCube({x:0,y:0,z:0},0.1);
+
+    let size = 2; 
+    
+    for (let i=-size; i<=size; i++) {
+        for (let j=-size; j<=size; j++) {
+            for (let k=-size; k<=size; k++) {
+                if (i==0 && j==0 && k==0) continue;
+                newCube({x: i*15, y: j*15, z: k*15}, 5);
+            }   
+        }
+    }
+
+    return {
+        get objects() {
+            return objects.map(calcObjPos);
+        },
+        rotateY: rotateY,
+        rotateX: rotateX,
+        move: move
+    }
+})();
+
+let rctx = (function(){
+    function renderObj(obj) {
+        ctx.strokeStyle = "white";
+        ctx.lineWidth = 4;
+        obj.lines.forEach(function(el){
+            if (obj.renderedPoints[el.from].z>0 && obj.renderedPoints[el.to].z>0) {
+                ctx.beginPath();
+                ctx.moveTo(obj.renderedPoints[el.from].x+canvas.width/2, obj.renderedPoints[el.from].y+canvas.height/2);
+                ctx.lineTo(obj.renderedPoints[el.to].x+canvas.width/2, obj.renderedPoints[el.to].y+canvas.height/2);
+                ctx.stroke();
+            }
+            
+        })  
+    }
+
+    function render(arr) {
+        ctx.clearRect(0,0,canvas.width, canvas.height)
+        arr.forEach(function(el){
+            renderObj(el);
+        });
+    }
+
+    return {
+        render: render
+    };
+})();
+
+let auxMove = {x:0, y:0, z:0}
+
+function draw() {
+    auxMove.x = 0, auxMove.y = 0, auxMove.z = 0;
+    rctx.render(environment.objects);
+    if (glob.pressedKeys['w']) auxMove.z+=1;
+    if (glob.pressedKeys['s']) auxMove.z-=1;
+    if (glob.pressedKeys['a']) auxMove.x-=1;
+    if (glob.pressedKeys['d']) auxMove.x+=1;
+    if (glob.pressedKeys['Shift']) auxMove.y-=1;
+    if (glob.pressedKeys[' ']) auxMove.y+=1;
+    environment.move({x:auxMove.x*glob.moveSensitivity, y:auxMove.y*glob.moveSensitivity, z:auxMove.z*glob.moveSensitivity});
+    window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
+
+canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
+canvas.addEventListener('click', canvas.requestPointerLock); 
+canvas.addEventListener('mousemove', function(e){
+    environment.rotateY(e.movementX*glob.sensitivity);
+    environment.rotateX(e.movementY*glob.sensitivity);
+});
+
+document.addEventListener('keydown', function(e){
+    glob.pressedKeys[e.key] = true;
+})
+
+document.addEventListener('keyup', function(e){
+    glob.pressedKeys[e.key] = false;
+})
+
+document.addEventListener('wheel', function(e){
+    glob.moveSensitivity -= e.deltaY/5000;
+    glob.moveSensitivity = Math.max(Math.min(3/4, glob.moveSensitivity), 1/100);
+})
